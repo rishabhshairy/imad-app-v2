edd7d634ce5d6332683bf98b168a246b5ac0f5de
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-06 00:01:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,70 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+
+var articleOne={
+    title: 'Rishabh Shairy Article one',
+    heading:'Article one',
+    date:'4-feb-2017',
+    content:`<p>
+                hey guyz this is my new website ........
+            </p>
+            <p>
+                learning server side javascript and linking websites through IMAD
+                
+            </p>`
+
+    
+};
+function createTemp(data){
+var title=data.title;
+var heading=data.heading;
+var date=data.date;
+var content=data.content;
+var htmlTemplate=`
+<!doctype html>
+<html>
+    <head> 
+    <title>
+        ${title}
+        
+    </title>
+    <meta name="viewport" content="width-device-width, initialscale=1"/>
+    <link href="/ui/style.css" rel="stylesheet" />
+    </head>
+    <body>
+    <div class="abc">
+        
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+         <div> 
+            ${heading}
+         </div>
+        </h3>
+        <div>
+           ${date}
+            
+        </div>
+        <div>
+           ${content}
+            
+            
+        </div>
+    </div>
+    </body>
+        
+</html>
+
+
+
+
+`;
+
+    return htmlTemplate;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
