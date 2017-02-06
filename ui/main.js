5b8e1bc4378a24284e6b2a9b44f5f6e0d3ce0123
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-07 00:28:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 console.log('Loaded!');
 //move the image
-var img=document.getElementById('new');
-img.onClick = function() {
+var imgObj=document.getElementById('new');
+imgObj.onClick = function() {
   img.style.marginLeft='200px';
 };
