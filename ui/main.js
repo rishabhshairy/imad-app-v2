2f515230bc9b55a915c034a15bfd7a58ec1d7b96
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-06 23:55:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,2 +1,8 @@
 console.log('Loaded!');
 alert('Enter your name to Continue');
+//move the image
+var img=document.getElementById('madi');
+img.onClick=function() 
+{
+  img.style.marginLeft='100px';
+};
