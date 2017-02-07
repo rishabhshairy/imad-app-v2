7506f0eab6548816bef39d74add1a8c9ba180e35
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-08 00:52:50 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -32,7 +32,7 @@
 				var list='';
 				for(var i=0;i<names.length;i++)
 				{
-					list+= '<li>'+ names +'</li>';
+					list+= '<li>'+ names[i] +'</li>';
 				}
 				var ul=document.getElementById('namelist');
 				ul.innerHTML=list;
