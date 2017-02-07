285c96398c9029c631b567a29a3bf160dd3f178b
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-08 00:44:54 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,3 +23,18 @@
 			      request.send(null); 
 			  
 			};
+			var nameInput=document.getElementById('name');
+			var name=nameInput.value;
+			var submit=document.getElementById('subtn');
+			submit.onclick=function() {
+			
+				var names=["name1","name2","name3","name4"];
+				var list='';
+				for(var i=0;i<names.length;i++)
+				{
+					list+= <li>+ names +</li>;
+				}
+				var ul=document.getElementById('namelist');
+				ul.innerHTML=list;
+				
+			};
