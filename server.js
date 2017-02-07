ce6b18a7010c2047b36d10ec03b401a1a7d88f76
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-07 21:32:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -134,6 +134,10 @@ app.get('/ui/WP_20160106_15_41_13_Pro.jpg', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'WP_20160106_15_41_13_Pro.jpg'));
 });
 
+app.get('/counter', function (req, res) {
+  counter=counter+1;
+  res.send(counter.toString());
+});
 
 
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
