994d4d64738ac1e72fed97113f4661f9fc6cb650
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-07 22:41:59 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -18,7 +18,7 @@ button.onclick = function() {
    }
       
   };
-  httpRequest.send('GET','http://rishabhshairy.imad.hasura-app.io',true);
+  httpRequest.open('GET','http://rishabhshairy.imad.hasura-app.io',true);
   httpRequest.send(null);
   
 };
