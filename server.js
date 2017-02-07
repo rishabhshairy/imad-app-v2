6c4866fb34a173687156f7005380a21e640b52fd
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-08 02:04:11 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -109,7 +109,7 @@ var htmlTemplate=`
 
 var names=[];
 app.get('/submit-name/:name',function(req,res){
-	var name=req.params.name;
+	var name=req.query.name;
 	names.push(name);
 	res.send(JSON.stringify(names));
 });
