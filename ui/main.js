77fe2d92fcde6a8748a480178b009b2f0a879e32
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-08 01:42:18 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -35,14 +35,15 @@
 			  request.onreadystatechange=function(){
 			    if(request.readyState===XMLHttpRequest.DONE){
 				    if(request.status===200){
-				        var names=["name1","name2","name3","name4"];
+				        var names=request.responseText;
+				        names=JSON.parse(name);
                             var list='';
                             	for(var i=0;i<names.length;i++)
-                                    				{
-                                    					list+= '<li>' + names + '</li>';
-                                    				}
-                                    				var ul=document.getElementById('namelist');
-                                    				ul.innerHTML=list;         
+                    				{
+                    					list+= '<li>' + names + '</li>';
+                    				}
+                    				var ul=document.getElementById('namelist');
+                    				ul.innerHTML=list;         
                                     			               
 				            
 				 }
@@ -52,7 +53,7 @@
 			        var nameInput=document.getElementById('name');
 					var name=nameInput.value;
 
-			    request.open('GET','http://rishabhshairy.imad.hasura-app.io/submit-name?name='+ name,true);
+			    request.open('GET','http://rishabhshairy.imad.hasura-app.io/submit-name?name=' + name,true);
 			   
 			      request.send(null); 
 			  
