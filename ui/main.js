1bcaf8147c5919d9a1272dd49efa6463315a61fd
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-07 20:03:00 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,11 @@
 console.log('Loaded!');
 var img = document.getElementById('new');
-img.onclick = function() {
-  
-  img.style.marginLeft=100;
-
+var marginLeft=0;
+function moveright(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft +'px';
+}
+img.onclick=function() {
     
+    var interval=setInterval(moveright,100);
 };
