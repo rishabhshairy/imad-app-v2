96867e32c6f1d2e23d6e4428d2309becb2a3e5d6
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-08 01:58:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -38,7 +38,7 @@
 				        var names=request.responseText;
 				        names=JSON.parse(names);
                             var list='';
-                            	for(var i=0;i<names.length;i++)
+                            	for(var i=0; i< names.length; i++)
                     				{
                     					list+= '<li>' + names[i] + '</li>';
                     				}
