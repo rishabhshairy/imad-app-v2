b5d61baca3dc13501b3d15561a1d5fba27c785b3
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-07 21:38:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,1 @@
-console.log('Loaded!');
-var img = document.getElementById('new');
-var marginLeft=0;
-function moveright(){
-    marginLeft=marginLeft+10;
-    img.style.marginLeft=marginLeft +'px';
-}
-img.onclick=function() {
-    
-    var interval=setInterval(moveright,100);
-};
+var buttton=document.getElementById('');
