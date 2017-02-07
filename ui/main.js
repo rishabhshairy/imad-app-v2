9ebedaeb737fe31ba7d726cb5e5226e43e63242a
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-08 01:29:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,16 +23,41 @@
 			      request.send(null); 
 			  
 			};
-			var nameInput=document.getElementById('name');
-			var name=nameInput.value;
 			var submit=document.getElementById('subtn');
 			submit.onclick=function() {
 			
-				var names=['name1','name2','name3','name4'];
+				//making request to server
+ 				var request=new XMLHttpRequest();
+			  
+			  
+			  
+			  request.onreadystatechange=function(){
+			    if(request.readyState===XMLHttpRequest.DONE){
+				    if(request.status===200){
+				            var counter=request.responseText;
+				   
+				            var span=document.getElementById('count');
+				            span.innerHTML=counter.toString();
+				 }
+			   }
+			      
+			  };
+			    var nameInput=document.getElementById('name');
+					var name=nameInput.value;
+
+			    request.open('GET','http://rishabhshairy.imad.hasura-app.io/counter',true);
+			   
+			      request.send(null); 
+			  
+			};
+
+
+				//capture the respone and store in variable
+				var names=["name1","name2","name3","name4"];
 				var list='';
-				for(var i=0; i<names.length;i++)
+				for(var i=0;i<names.length;i++)
 				{
-					list+='<li>' + names[i] + '</li>';
+					list+= '<li>' + names + '</li>';
 				}
 				var ul=document.getElementById('namelist');
 				ul.innerHTML=list;
