0ec7b6455354c03df39befea119030e7233e1cbf
Rishabh Shairy <rishusinha8@gmail.com>
2017-02-07 22:15:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -136,7 +136,7 @@ app.get('/ui/WP_20160106_15_41_13_Pro.jpg', function (req, res) {
 });
 
 var counter=0;
-app.get('/counter', function (req, res) {
+app.get('/counter' , function (req, res) {
   counter=counter+1;
   res.send(counter.toString());
 });
