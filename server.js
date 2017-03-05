ef27a7e81ca4ade1aca4b98d7d409092334d70bd
Rishabh Shairy <rishusinha8@gmail.com>
2017-03-05 17:08:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -156,7 +156,7 @@ app.get('/test-db',function(req,res){
             res.status(500).send(err.toString());
         }
         else{
-            res.send(JSON.stringify(result));
+            res.send(JSON.stringify(result.row));
         }
     });
     
