f059e5f3b0c0555edb8b51e23a6c43a82677c936
Rishabh Shairy <rishusinha8@gmail.com>
2017-03-05 17:02:32 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool=require('pg').Pool;
+
+var config={
+    user:'rishabhshairy',
+    databas:'rishabhshairy',
+    host:'db.imad.hasura-app.io',
+    port:'5432',
+    password:process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -136,6 +145,23 @@ app.get('/counter' , function (req, res) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+
+var pool = new Pool(config);
+app.get('/test-db',function(req,res){
+    //make a request
+    //return a response
+    pool.query('SELECT * FROM article',function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send(JSON.stringify(result));
+        }
+    });
+    
+});
+
 
 app.get('/:articleName',function(req,res){
     var articleName = req.params.articleName;
