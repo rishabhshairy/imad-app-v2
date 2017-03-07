32704ab37f1fd7f83b93633d0ad5008e685c41f3
Rishabh Shairy <rishusinha8@gmail.com>
2017-03-07 22:22:20 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,5 @@
-	var button=document.getElementById('counter');
-			button.onclick=function() {
-			  
-			 
-			  
-			  var request=new XMLHttpRequest();
-			  
-			  
-			  
-			  request.onreadystatechange=function(){
-			    if(request.readyState===XMLHttpRequest.DONE){
-				    if(request.status===200){
-				            var counter=request.responseText;
-				   
-				            var span=document.getElementById('count');
-				            span.innerHTML=counter.toString();
-				 }
-			   }
-			      
-			  };
-			    request.open('GET','http://rishabhshairy.imad.hasura-app.io/counter',true);
-			   
-			      request.send(null); 
-			  
-			};
-			
-		    //names code
+
+		    //username/password code
 			var submit=document.getElementById('subtn');
 			submit.onclick=function() {
 			
@@ -38,26 +13,26 @@
 				    
 				    if(request.status===200){
 				        
-				        var names=request.responseText;
-				        names=JSON.parse(names);
-                            var list='';
-                            	for(var i=0; i< names.length; i++)
-                    				{
-                    					list+= '<li>' + names[i] + '</li>';
-                    				}
-                    		var ul=document.getElementById('namelist');
-                    		ul.innerHTML=list;         
-                                    			               
-				            
-				 }
+				        console.log('User Logged In');
+				        alert('login succesfull');
+                    }
+                    else if(request.status===403){
+                        alert('Invalid username/password');
+                    }
+                    else if(request.status===500)
+                    {
+                        alert('Server Internal Error');
+                    }
 			   }
 			      
 			  };
-			        var nameInput=document.getElementById('name');
-					var name=nameInput.value;
+			        var username=document.getElementById('username').value;
+			        var password=document.getElementById('password').value;
+			        console.log(username);
+			        console.log(password);
 
-			           request.open('GET','http://rishabhshairy.imad.hasura-app.io/submit-name?name=' + name,true);
-			   
-			      request.send(null); 
+			           request.open('POST','http://rishabhshairy.imad.hasura-app.io/login',true);
+			           request.setRequestHeader('Content-Type','application/json');
+			           request.send(JSON.stringify({username:username,password:password})); 
 			  
 			};
