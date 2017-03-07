b8f3d9ca0024be3e1bc3703746f2d4923d416e6c
Rishabh Shairy <rishusinha8@gmail.com>
2017-03-07 20:31:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -98,6 +98,26 @@ app.get('/hash/:input',function(req,res){
     
 });
 
+app.post('/create-user',function(req,res){
+   //username,password
+    //JSON "username":rishabh;"password"="qwerty"
+   var username=req.body.username;
+   var passsword=req.body.password;
+   
+   var salt=crypto.randomBytes(128).toString('hex');
+   var dbString=hash(input,salt);
+   pool.query('INSERT INTO "user" (username,password) VALUES($1,$2)',[username,dbString],function(err,result){
+        if(err){
+            res.status(500).send(err.toString());
+        }
+        else{
+            res.send('User successfully created '+ username);
+        }
+       
+   });
+    
+});
+
 var pool = new Pool(config);
 app.get('/test-db',function(req,res){
     //make a request
